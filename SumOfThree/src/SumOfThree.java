99c7aa369b968fb06d384f630b7510679f14a41f
HAMEED MOHAMMAD <HAMEED MOHAMMAD>
2017-01-14 18:31:04 -0600

    Added sumofthree folder

---
+++ SumOfThree/src/SumOfThree.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class SumOfThree {
+	
+	int a;
+	int b;
+	int c;
+	
+	public static int sumofthree(int a, int b, int c)
+	{
+		int result;
+		
+		result = a+b+c;
+		
+		return result;
+		
+	}
+	
+	
+	public static void main(String [] args)
+	{
+		int num1,num2,num3;
+		System.out.println("Enter any 3 integers ");
+		Scanner in = new Scanner(System.in);
+		num1 = in.nextInt();
+		num2 = in.nextInt();
+		num3 = in.nextInt();
+		
+		int result = sumofthree(num1, num2, num3);
+		System.out.println("Sum of three numbers: " +result );
+		
+	}
+
+}
